d1d429769f7aff0242c58588e026cc0d3217a836
fikadu-girma <fikadugirma690@gmail.com>
2026-01-09 21:55:56 -0800

    Refactor SecureFileRead class to enhance file path security checks

--- SecureFileRead.java
+++ SecureFileRead.java
@@ -1,3 +1,20 @@
+import java.io.File;
+import java.io.IOException;
+
 public class SecureFileRead {
-    
+
+    public static void readFile(String baseDir, String fileName) throws IOException {
+        File base = new File(baseDir).getCanonicalFile();
+        File file = new File(base, fileName).getCanonicalFile();
+
+        if (!file.getPath().startsWith(base.getPath())) {
+            throw new SecurityException("Path traversal attempt detected!");
+        }
+
+        System.out.println("Safe file path: " + file.getPath());
+    }
+
+    public static void main(String[] args) throws Exception {
+        readFile("/safe/data", "example.txt");
+    }
 }
