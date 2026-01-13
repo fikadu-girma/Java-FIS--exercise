1ab99141075b43c4c46b9cf22a2c20a2c850eb5c
fikadu-girma <fikadugirma690@gmail.com>
2026-01-12 22:15:39 -0800

    Add SecureFileEncryption class to implement AES file encryption

---
+++ Complecated/SecureFileEncryption.java
@@ -0,0 +1,27 @@
+import javax.crypto.*;
+import java.io.*;
+import java.security.SecureRandom;
+
+public class SecureFileEncryption {
+
+    public static void encryptFile(File input, File output, SecretKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+
+        try (FileInputStream fis = new FileInputStream(input);
+             FileOutputStream fos = new FileOutputStream(output);
+             CipherOutputStream cos = new CipherOutputStream(fos, cipher)) {
+
+            fis.transferTo(cos);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        KeyGenerator kg = KeyGenerator.getInstance("AES");
+        kg.init(128, new SecureRandom());
+        SecretKey key = kg.generateKey();
+
+        encryptFile(new File("plain.txt"), new File("encrypted.bin"), key);
+        System.out.println("File encrypted");
+    }
+}
