6a62c926e78dca36146d887ddc1942c704443d9f
fikadu-girma <fikadugirma690@gmail.com>
2026-01-12 22:14:20 -0800

    Add PBKDF2PasswordHashing class to implement secure password hashing

---
+++ Complecated/PBKDF2PasswordHashing.java
@@ -0,0 +1,22 @@
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class PBKDF2PasswordHashing {
+
+    public static String hashPassword(char[] password, byte[] salt) throws Exception {
+        PBEKeySpec spec = new PBEKeySpec(password, salt, 65536, 256);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        byte[] hash = skf.generateSecret(spec).getEncoded();
+        return Base64.getEncoder().encodeToString(hash);
+    }
+
+    public static void main(String[] args) throws Exception {
+        byte[] salt = new byte[16];
+        new SecureRandom().nextBytes(salt);
+
+        String hash = hashPassword("StrongPass123!".toCharArray(), salt);
+        System.out.println("Password Hash: " + hash);
+    }
+}
